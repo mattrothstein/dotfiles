ca5eb4895cbfbce37dc0ca8c1de39e783a7485e5
Matt Rothstein <matthewrothstein@hotmail.com>
2017-11-22 10:44:55 -0500

    weekly update

--- push_dotfiles.sh
+++ push_dotfiles.sh
@@ -1,2 +1,2 @@
 #git add, commit and push dot files
-cd ~ && env -i git add -f .zshrc && env -i git add -f .pryrc && env -i git add -f .bash_profile && env -i git add -f .vimrc && env -i git add -f push_dotfiles.sh && env -i git commit -m "weekly update" && git push origin master
+cd ~ && env -i git add -f .zshrc && env -i git add -f .pryrc && env -i git add -f .bash_profile && env -i git add -f .vimrc && env -i git add -f push_dotfiles.sh && env -i git -c "user.name=Matt Rothstein" -c "user.email=matthewrothstein@hotmail.com" commit -m "weekly update" && git push origin master
